94efba0524b28163c6c22eef92a71f989bf573e8
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-06 13:34:24 +0800

    feat: Add console for debugging

---
+++ KeyLogger.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace KeyLogger.Console
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        static void Main(string[] args)
+        {
+            int processId = 263718593;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(UnhookWindowsHookEx(processId));
+        }
+    }
+}
