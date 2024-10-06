bcfc2c7a66cbf1647fa4e62c19b51759f80cbe77
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-06 10:39:44 +0800

    initial

---
+++ KeyLogger/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show(textBlock1.Text);
+            if (e.Key == Key.Return)
+            {
+                textBlock1.Text = "You Entered: " + textBox1.Text;
+            }
+        }
+    }
+}
