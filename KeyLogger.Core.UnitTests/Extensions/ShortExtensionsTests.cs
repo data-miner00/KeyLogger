c9715664eea82f1ed0ddb6df3233bcb4f8d6361f
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-09 14:00:37 +0800

    test: Add first bit turned on tests

---
+++ KeyLogger.Core.UnitTests/Extensions/ShortExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace KeyLogger.Core.UnitTests.Extensions;
+
+using KeyLogger.Core.Extensions;
+using Xunit;
+
+public sealed class ShortExtensionsTests
+{
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-32768)]
+    public void FirstBitIsTurnedOn_FirstBitTurnedOn_ReturnTrue(short value)
+    {
+        var result = value.FirstBitIsTurnedOn();
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(32767)]
+    public void FirstBitIsTurnedOn_FirstBitTurnedOff_ReturnFalse(short value)
+    {
+        var result = value.FirstBitIsTurnedOn();
+        Assert.False(result);
+    }
+}
