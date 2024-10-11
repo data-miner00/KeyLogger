a0e156f9b929fac21fe13d2e98faaeab25a7297f
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-11 18:55:04 +0800

    test: Add fixed sized queue tests

---
+++ KeyLogger.Core.UnitTests/FixedSizedQueueSteps.cs
@@ -0,0 +1,79 @@
+﻿namespace KeyLogger.Core.UnitTests;
+
+using FluentAssertions;
+using KeyLogger.Core;
+using System.Collections.Generic;
+
+public sealed class FixedSizedQueueSteps
+{
+    private const int MaximumSize = 5;
+    private readonly FixedSizedQueue<int?> fixedSizedQueue;
+
+    public FixedSizedQueueSteps()
+    {
+        this.fixedSizedQueue = new(MaximumSize);
+    }
+
+    public FixedSizedQueueSteps WhenIEnqueueItem(int item)
+    {
+        this.fixedSizedQueue.Enqueue(item);
+        return this;
+    }
+
+    public FixedSizedQueueSteps WhenIEnqueueItems(IEnumerable<int> items)
+    {
+        foreach (var item in items)
+        {
+            this.fixedSizedQueue.Enqueue(item);
+        }
+
+        return this;
+    }
+
+    public FixedSizedQueueSteps WhenIClearQueue()
+    {
+        this.fixedSizedQueue.Clear();
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectQueueItemsToBeEmpty()
+    {
+        this.fixedSizedQueue.GetAll.Should().NotBeNull();
+        this.fixedSizedQueue.GetAll.Should().BeEmpty();
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectQueueItemsToBe(List<int> items)
+    {
+        this.fixedSizedQueue.GetAll.Should().NotBeNull();
+        this.fixedSizedQueue.GetAll.Count.Should().Be(items.Count);
+        this.fixedSizedQueue.GetAll.Should().BeEquivalentTo(items);
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectFirstItemToBe(int item)
+    {
+        this.fixedSizedQueue.Peek.Should().NotBeNull();
+        this.fixedSizedQueue.Peek.Should().Be(item);
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectLastItemToBe(int item)
+    {
+        this.fixedSizedQueue.PeekLast.Should().NotBeNull();
+        this.fixedSizedQueue.PeekLast.Should().Be(item);
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectFirstItemToBeNull()
+    {
+        this.fixedSizedQueue.Peek.Should().BeNull();
+        return this;
+    }
+
+    public FixedSizedQueueSteps ThenIExpectLastItemToBeNull()
+    {
+        this.fixedSizedQueue.PeekLast.Should().BeNull();
+        return this;
+    }
+}
---
+++ KeyLogger.Core.UnitTests/FixedSizedQueueTests.cs
@@ -0,0 +1,54 @@
+﻿namespace KeyLogger.Core.UnitTests;
+
+using System.Collections.Generic;
+
+public sealed class FixedSizedQueueTests
+{
+    private readonly FixedSizedQueueSteps steps = new();
+
+    [Fact]
+    public void Initialize_ItemsShouldBeEmpty()
+    {
+        this.steps
+            .ThenIExpectQueueItemsToBeEmpty()
+            .ThenIExpectFirstItemToBeNull()
+            .ThenIExpectLastItemToBeNull();
+    }
+
+    [Fact]
+    public void Enqueue_Enqueue1Item_FirstAndLastShouldEqual()
+    {
+        var number = 5;
+
+        this.steps
+            .WhenIEnqueueItem(number)
+            .ThenIExpectFirstItemToBe(number)
+            .ThenIExpectLastItemToBe(number)
+            .ThenIExpectQueueItemsToBe([number]);
+    }
+
+    [Fact]
+    public void Enqueue_Enqueue5Items_EnqueuedSuccessfully()
+    {
+        List<int> numbers = [5, 1, 2, 3, 4];
+
+        this.steps
+            .WhenIEnqueueItems(numbers)
+            .ThenIExpectFirstItemToBe(5)
+            .ThenIExpectLastItemToBe(4)
+            .ThenIExpectQueueItemsToBe(numbers);
+    }
+
+    [Fact]
+    public void Clear_ExistingQueue_ShouldBeEmptied()
+    {
+        List<int> numbers = [5, 1, 2, 3, 4];
+
+        this.steps
+            .WhenIEnqueueItems(numbers)
+            .WhenIClearQueue()
+            .ThenIExpectQueueItemsToBeEmpty()
+            .ThenIExpectFirstItemToBeNull()
+            .ThenIExpectLastItemToBeNull();
+    }
+}
