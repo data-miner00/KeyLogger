c9f650b632534eae0cd4ccaacd37a69f67782c07
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-13 20:35:25 +0800

    feat: IoC container

--- KeyLogger/App.xaml.cs
+++ KeyLogger/App.xaml.cs
@@ -1,12 +1,45 @@
 ﻿namespace KeyLogger;
 
-using System.Configuration;
-using System.Data;
+using Autofac;
+using KeyLogger.View;
+using System;
 using System.Windows;
 
 /// <summary>
 /// Interaction logic for App.xaml.
 /// </summary>
-public partial class App : Application
+public sealed partial class App : Application, IDisposable
 {
+    private readonly IContainer container = ContainerConfig.Configure();
+
+    private bool isDisposed;
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (this.isDisposed)
+        {
+            return;
+        }
+
+        this.container.Dispose();
+        this.isDisposed = true;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainWindow = this.container.Resolve<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    /// <inheritdoc/>
+    protected override void OnExit(ExitEventArgs e)
+    {
+        this.Dispose();
+
+        base.OnExit(e);
+    }
 }
---
+++ KeyLogger/ContainerConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace KeyLogger;
+
+using Autofac;
+using KeyLogger.View;
+
+/// <summary>
+/// IoC container configuration definition.
+/// </summary>
+internal static class ContainerConfig
+{
+    /// <summary>
+    /// Configures the IoC container.
+    /// </summary>
+    /// <returns>The configured IoC container.</returns>
+    public static IContainer Configure()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterType<MainWindow>().SingleInstance();
+
+        return builder.Build();
+    }
+}
